f941dc0b39dfd36ce0aac9abf5c630580dd80c95
royel21 <royelconsoro@hotmail.com>
2020-03-06 14:30:41 -0400

    initial changes

---
+++ src/Components/Login.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Login extends Component {
+  render() {
+    return <React.Fragment></React.Fragment>;
+  }
+}
+
+export default Login;
---
+++ src/Components/Mangas.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Mangas extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h1>All Mangas</h1>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Mangas;
